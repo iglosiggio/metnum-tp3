24382b045c59f6123e5a6ad92049bd3b94bd571a
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2021-11-07 21:50:38 -0300

    Agregar cuadrados mínimos usando las ecuaciones normales

---
+++ cuadrados_minimos/metnum.cpp
@@ -0,0 +1,29 @@
+#include <tuple>
+#include <Eigen/Sparse>
+#include <Eigen/Dense>
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+namespace py=pybind11;
+
+using uint = unsigned int;
+
+using Eigen::MatrixXd;
+using Matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+using SparseMatrix = Eigen::SparseMatrix<double>;
+using Vector = Eigen::VectorXd;
+
+// https://eigen.tuxfamily.org/dox-devel/group__LeastSquares.html#title2
+Vector least_squares(const Matrix& a, const Matrix& b) {
+	Vector v = (a.transpose() * a).ldlt().solve(b);
+	return v;
+}
+
+PYBIND11_MODULE(metnum, m) {
+	m.def(
+		"least_squares", &least_squares,
+		"Calculates the solution to a least-squares problem",
+		py::arg("a"),
+		py::arg("b")
+	);
+}
