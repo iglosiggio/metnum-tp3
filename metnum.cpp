fb77c731e2ec5fbafca042725c54d4e37927daad
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2021-11-07 22:25:32 -0300

    Me olvidé de multiplicar v por a^t

--- metnum.cpp
+++ metnum.cpp
@@ -1,21 +1,15 @@
-#include <tuple>
-#include <Eigen/Sparse>
 #include <Eigen/Dense>
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 
 namespace py=pybind11;
 
-using uint = unsigned int;
-
-using Eigen::MatrixXd;
-using Matrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-using SparseMatrix = Eigen::SparseMatrix<double>;
+using Matrix = Eigen::MatrixXd;
 using Vector = Eigen::VectorXd;
 
 // https://eigen.tuxfamily.org/dox-devel/group__LeastSquares.html#title2
-Vector least_squares(const Matrix& a, const Matrix& b) {
-	return (a.transpose() * a).ldlt().solve(b);
+Vector least_squares(const Matrix& a, const Vector& b) {
+	return (a.transpose() * a).ldlt().solve(a.transpose() * b);
 }
 
 PYBIND11_MODULE(metnum, m) {
